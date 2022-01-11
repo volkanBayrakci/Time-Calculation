82401fb6e5136ec6a7bd1492143d312318e338de
volkanBayrakci <volkanbayrakci97@gmail.com>
2022-01-12 00:58:30 +0300

    Uygulama Tamamlandı,Bootstrap yüklendi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [hour, setHour] = useState();
+  const [minute, setMinute] = useState();
+  const [pause, setPause] = useState();
+  const [result, setResult] = useState();
+  const [resultTwo, setResultTwo] = useState();
+
+  const handleResult = () => {
+    const saat = hour * 60;
+    const dakika = (minute / 60) * 60;
+    const birlestir = saat + dakika - pause;
+    const cikar = (saat + dakika - pause) / 60;
+    setResult(birlestir);
+    setResultTwo(cikar);
+  };
+
+  const handleReset = (e) => {
+    e.preventDefault();
+    setHour("");
+    setMinute("");
+    setPause("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container mt-3">
+      <form>
+        <div className="form-group">
+          <label htmlFor="hour">Saat : </label>
+          <input
+            type="number"
+            className="form-control"
+            id="hour"
+            placeholder="Saati Giriniz"
+            value={hour}
+            onChange={(e) => setHour(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="minute">Dakika : </label>
+          <input
+            type="number"
+            className="form-control"
+            id="minute"
+            placeholder="Dakikayı Giriniz"
+            value={minute}
+            onChange={(e) => setMinute(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="minute">Mola : </label>
+          <input
+            type="number"
+            className="form-control"
+            id="minute"
+            placeholder="Molayı Giriniz"
+            value={pause}
+            onChange={(e) => setPause(e.target.value)}
+          />
+        </div>
+      </form>
+      <div className="mt-2">
+        <button
+          className="btn btn-success"
+          onClick={handleResult}
+          style={{ marginRight: "5px" }}
         >
-          Learn React
-        </a>
-      </header>
+          Sonuç
+        </button>
+        <button onClick={handleReset} className="btn btn-danger">
+          Sıfırla
+        </button>
+      </div>
+      <div className="mt-3 text-center">
+        {resultTwo && resultTwo + " " + "Saat"}
+      </div>
+      <div className="mt-3 text-center">
+        {result && result + " " + "Dakika"}
+      </div>
+      <hr />
     </div>
   );
 }
-
-export default App;
