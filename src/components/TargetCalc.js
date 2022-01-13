50dbe636d76b9a265421d6ed448fb06d428178dc
volkanBayrakci <volkanbayrakci97@gmail.com>
2022-01-14 02:27:26 +0300

    Uygulama Componentlere ayrıldı

---
+++ src/components/TargetCalc.js
@@ -0,0 +1,83 @@
+import { useState } from "react"
+
+
+const TargetCalc = () => {
+  const [employee, setEmployee] = useState();
+  const [target, setTarget] = useState();
+  const [resultThree, setResultThree] = useState();
+  const [resultFour, setResultFour] = useState();
+
+
+  const handleClick = () => {
+    const employeeVariable = employee
+    const targetVariable = target
+    const combining = targetVariable / 60 / employeeVariable / 7.25
+    const calculation = combining * 435
+    setResultThree(combining)
+    setResultFour(calculation.toFixed(0))
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    setEmployee("")
+    setTarget("")
+  }
+
+  return (
+    <>
+      <form>
+        <div className="form-group">
+          <label htmlFor="employee">Personel : </label>
+          <input
+            type="number"
+            className="form-control"
+            id="employee"
+            placeholder="Personel Sayısını Giriniz"
+            value={employee}
+            onChange={(e) => setEmployee(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="target">Hedef : </label>
+          <input
+            type="number"
+            className="form-control"
+            id="target"
+            placeholder="Hedefi Giriniz"
+            value={target}
+            onChange={(e) => setTarget(e.target.value)}
+          />
+        </div>
+      </form>
+      <div className="mt-2">
+        <button
+          className="btn btn-success"
+          onClick={handleClick}
+          style={{ marginRight: "5px" }}
+          disabled={!employee || !target}
+        >
+          Sonuç
+        </button>
+        <button onClick={handleReset} className="btn btn-danger">
+          Sıfırla
+        </button>
+      </div>
+      <div className="row mt-2">
+        <div className="col-md-6 col-sm-6 col-6">
+          <label className="w-100" >
+            Dakika Hedefi:
+            <input type="number" value={resultThree} className="form-control" />
+          </label>
+        </div>
+        <div className="col-md-6 col-sm-6 col-6">
+          <label className="w-100">
+            Gün Sonu Hedefi:
+            <input type="number" value={resultFour} className="form-control" />
+          </label>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default TargetCalc
---
+++ src/components/TimeCalc.js
@@ -0,0 +1,108 @@
+import { useState } from "react"
+
+const TimeCalc = () => {
+  const [hour, setHour] = useState();
+  const [minute, setMinute] = useState();
+  const [pause, setPause] = useState(null);
+  const [shift, setShift] = useState();
+  const [result, setResult] = useState();
+  const [resultTwo, setResultTwo] = useState();
+
+
+  const handleResult = () => {
+    // const saat = hour * 60;
+    // const dakika = (minute / 60) * 60;
+    // const birlestir = saat + dakika - pause;
+    // const cikar = (saat + dakika - pause) / 60;
+    // setResult(birlestir);
+    // setResultTwo(cikar);
+    const calculation = (hour - shift) * 60 + (minute - pause);
+    const result = calculation / 60;
+    setResult(calculation);
+    setResultTwo(result);
+  };
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    setHour("")
+    setMinute("")
+  }
+
+  return (
+    <>
+      <form>
+        <div className="form-row">
+          <div className="form-group">
+            <label htmlFor="hour">Saat : </label>
+            <input
+              type="number"
+              className="form-control"
+              id="hour"
+              placeholder="Saati Giriniz"
+              value={hour}
+              onChange={(e) => setHour(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="minute">Dakika : </label>
+            <input
+              type="number"
+              className="form-control"
+              id="minute"
+              placeholder="Dakikayı Giriniz"
+              value={minute}
+              onChange={(e) => setMinute(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="pause">Mola : </label>
+            <input
+              type="number"
+              className="form-control"
+              id="pause"
+              placeholder="Molayı Giriniz"
+              value={pause}
+              onChange={(e) => setPause(e.target.value)}
+            />
+          </div>
+          <select class="form-select mt-3" onChange={(e) => setShift(e.target.value)}>
+            <option selected>Vardiya Seçiniz</option>
+            <option value="7">7</option>
+            <option value="15">15</option>
+            <option value="23">23</option>
+            <option value="19">19</option>
+          </select>
+        </div>
+      </form>
+      <div className="mt-2">
+        <button
+          className="btn btn-success"
+          onClick={handleResult}
+          style={{ marginRight: "5px" }}
+          disabled={!hour || !minute}
+        >
+          Sonuç
+        </button>
+        <button onClick={handleReset} className="btn btn-danger">
+          Sıfırla
+        </button>
+      </div>
+      <div className="row mt-2">
+        <div className="col-md-6 col-sm-6 col-6">
+          <label className="w-100">
+            Saat:
+            <input type="number" value={resultTwo} className="form-control" />
+          </label>
+        </div>
+        <div className="col-md-6 col-dm-6 col-6">
+          <label className="w-100">
+            Dakika:
+            <input type="number" value={result} className="form-control" />
+          </label>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default TimeCalc
